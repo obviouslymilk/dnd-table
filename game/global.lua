002ac9ae66910603a4b98864199e2c3b7a6f6f17
Klim Storozhenko <68001894+obviouslymilk@users.noreply.github.com>
2024-02-25 15:47:22 +0300

    Add lunajson module

--- game/global.lua
+++ game/global.lua
@@ -1,4 +1,5 @@
 local characterSheetUI = require("ui.character_sheet")
+local lunajson = require('lib.lunajson')
 
 local UNITERACTABLE_GUID = {
     "8f9d8c",
@@ -50,4 +51,5 @@ function onLoad()
     setUninteractable()
     setupSheets()
     hideGamemasterZones("eaef99","6192e7")
+    local dude = lunajson.encode({})
 end
