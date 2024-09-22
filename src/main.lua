927decc2c38734d3e6e6858b9374c967ca6eb663
Klim <storozhenko.klim@gmail.com>
2024-09-22 11:32:30 +0300

    prototyping

--- src/main.lua
+++ src/main.lua
@@ -3,10 +3,12 @@ local characterSheet = require('ui.character')
 
 local FONT_CUSTOM_ASSETS = {
     {
+        type = 1,
         name = "bold",
         url = "http://cloud-3.steamusercontent.com/ugc/2333495943854237911/8287105B99E942AF915AA0D53285BBBADCB13E33/"
     },
     {
+        type = 1,
         name = "regular",
         url = "http://cloud-3.steamusercontent.com/ugc/2333496578698759898/3F417EDC5CF86F7B9BBFF71066F043672154EE4D/"
     }
--- src/ui/character.lua
+++ src/ui/character.lua
@@ -11,10 +11,10 @@ return [[
         returnToOriginalPositionWhenReleased="false"
     >
         <HorizontalLayout spacing="12" childForceExpandHeight="false">
-            <Button minHeight="50" onClick="SelectPanel(${color})">Персонаж</Button>
-            <Button minHeight="50" onClick="SelectPanel(${color})">Инвентарь</Button>
+            <Button minHeight="50" onClick="SelectPanel(${color})" font="bold">Персонаж</Button>
+            <Button minHeight="50" onClick="SelectPanel(${color})" font="bold">Инвентарь</Button>
         </HorizontalLayout>
-        <Text id="${color}_char" font="bold">Лист персонажа</Text>
-        <Text id="${color}_inv" active="false">Инвентарь персонажа</Text>
+        <Text id="${color}_char" font="regular">Лист персонажа</Text>
+        <Text id="${color}_inv" active="false" font="regular">Инвентарь персонажа</Text>
     </VerticalLayout>
 ]]
