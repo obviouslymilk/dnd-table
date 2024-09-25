46710579d5bb32c51c7340e4fabe6c62c4d65cde
Klim <storozhenko.klim@gmail.com>
2024-09-25 22:27:21 +0300

    sheet: make draggable out of bounds.

--- src/ui/character-sheet.lua
+++ src/ui/character-sheet.lua
@@ -4,6 +4,7 @@ return [[
         height="700"
         allowDragging="true"
         returnToOriginalPositionWhenReleased="false"
+        restrictDraggingToParentBounds="false"
     >
         <VerticalLayout ignoreLayout="true" offsetXY="271 0" height="650" width="42" color="none" childForceExpandHeight="false" spacing="6">
             <Button class="sheet-tab" tooltip="Персонаж" icon="char-icon"/>
