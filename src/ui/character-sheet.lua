992f1e7a9fc5ad4f4b1e61f7022768be0a6a718b
Klim <storozhenko.klim@gmail.com>
2024-09-26 23:54:02 +0300

    add input fields

--- src/ui/character-sheet.lua
+++ src/ui/character-sheet.lua
@@ -11,6 +11,7 @@ return [[
         restrictDraggingToParentBounds="false"
         id="${color}_sheet"
         visibility="${color}"
+        color="#ffffff"
     >
         <VerticalLayout ignoreLayout="true" offsetXY="271 0" height="650" width="42" color="none" childForceExpandHeight="false" spacing="6">
             <Button class="sheet-tab" tooltip="Персонаж" icon="char-icon"/>
@@ -19,7 +20,27 @@ return [[
         </VerticalLayout>
 
         <Panel image="cover" height="100" raycastTarget="false" rectAlignment="UpperLeft">
-            <Text color="white" font="bold" fontSize="20">ИМЯ ПЕРСОНАЖА</Text>
+            <HorizontalLayout>
+                <VerticalLayout childForceExpandHeight="false" childAlignment="MiddleCenter">
+                    <InputField class="sheet-header-input"
+                        placeholderText="ИМЯ ПЕРСОНАЖА"
+                        font="bold"
+                        fontSize="20"
+                        textAlignment="LowerLeft"
+                        characterLimit="20"
+                        textOffset="20 0 0 0"
+                    />
+                    <InputField
+                        class="sheet-header-input"
+                        placeholder="Класс Уровень (Воин 1)"
+                        textAlignment="UpperLeft"
+                        characterLimit="20"
+                        textOffset="20 0 0 0"
+                        textColor="#a3a3a3"
+                    />
+                </VerticalLayout>
+                <Panel></Panel>
+            </HorizontalLayout>
         </Panel>
 
         <HorizontalLayout padding="12 12 24 12" spacing="12" height="600" rectAlignment="LowerCenter">
--- src/ui/defaults.lua
+++ src/ui/defaults.lua
@@ -2,10 +2,9 @@ return [[
     <Defaults>
         <Text color="#09090b" fontSize="14" tooltipBackgroundColor="black"/>
         <Button font="bold" />
-        <Panel color="#ffffff" />
-        <HorizontalLayout color="#ffffff" />
-        <VerticalLayout color="#ffffff" />
 
         <Button class="sheet-tab" colors="#ffffff|#e4e4e7" image="sheet-btn" iconWidth="25" minHeight="42"  />
+
+        <InputField class="sheet-header-input" color="none" textColor="#fafafa" font="regular" selectionColor="#27272a" />
     </Defaults>
 ]]
